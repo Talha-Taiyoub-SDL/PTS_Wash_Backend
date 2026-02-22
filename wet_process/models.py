3731738ded2ec41ad7321fe064dedb364c67406e
Talha Taiyoub <talha.taiyoub@gmail.com>
2026-02-22 14:53:53 +0600

    Create model BatchForFirstWash

--- wet_process/models.py
+++ wet_process/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class BatchForFirstWash(models.Model):
+    shade = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=100)
+    total_quantity = models.IntegerField(default=0)
+    status  = models.CharField(max_length=100,null=True,blank=True) # If it's needed later
+    
+    
