f8bdd3d2827a75a12b13603bf091e9e79f59a87c
Talha Taiyoub <talha.taiyoub@gmail.com>
2026-02-24 11:28:01 +0600

    optimize the api when the source is bundle

--- wet_process/serializers.py
+++ wet_process/serializers.py
@@ -1,6 +1,6 @@
 from django.db import transaction
 from production.models import Batch,ReceivedBundle
-from production.serializers import get_user_name
+from production.serializers import get_user_name,ReceivedBundleSerializer
 from .models import BatchForFirstWash,FirstWashBatchSource,FirstWashBundleSource
 from rest_framework import serializers
 
@@ -20,10 +20,17 @@ class BundleSourceItemSerializer(serializers.Serializer):
 class FirstWashBatchSourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = FirstWashBatchSource
-        fields = ["batch","quantity"]    
+        fields = ["batch","quantity"]
+            
+class FirstWashBundleSourceSerializer(serializers.ModelSerializer):
+    bundle = ReceivedBundleSerializer()
+    class Meta:
+        model = FirstWashBundleSource
+        fields = ["bundle","quantity"]    
     
 class BatchForFirstWashSerializer(serializers.ModelSerializer):
     source_batches = FirstWashBatchSourceSerializer(many=True,read_only=True)
+    source_bundles = FirstWashBundleSourceSerializer(many=True, read_only=True)
     batch_source = BatchSourceItemSerializer(many=True, write_only=True, required = False)
     bundle_source = BundleSourceItemSerializer(
         many=True, write_only=True, required=False
@@ -31,19 +38,20 @@ class BatchForFirstWashSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = BatchForFirstWash
-        fields = ["id","shade","created_at","created_by","total_quantity","status","batch_source","bundle_source","source_batches"]
+        fields = ["id","shade","created_at","created_by","total_quantity","status","batch_source","bundle_source","source_batches","source_bundles"]
         read_only_fields = ["created_by","total_quantity","status"]
-    
-    # validate that only one source is provided    
+       
     def validate(self, attrs):
         batch_source = attrs.get("batch_source")
         bundle_source = attrs.get("bundle_source")
 
+         # validate that only one source is provided 
         if batch_source and bundle_source:
             raise serializers.ValidationError(
                 "Provide either batch_source or bundle_source, not both."
             )
 
+         # validate that at least one source is provided 
         if not batch_source and not bundle_source:
             raise serializers.ValidationError(
                 "You must provide either batch_source or bundle_source."
@@ -59,11 +67,13 @@ class BatchForFirstWashSerializer(serializers.ModelSerializer):
         total_quantity = 0
         
         with transaction.atomic():
+            # create the batch
             batch_for_first_wash = BatchForFirstWash.objects.create(
                 created_by=created_by,
                 **validated_data
             )
 
+            #  when the source is batch
             if batch_source_data:
                 total_quantity = sum(item["quantity"] for item in batch_source_data)
 
@@ -74,15 +84,25 @@ class BatchForFirstWashSerializer(serializers.ModelSerializer):
                         quantity=item["quantity"],
                     )
 
+            # when the source is bundle
             if bundle_source_data:
                 total_quantity = sum(item["quantity"] for item in bundle_source_data)
 
                 for item in bundle_source_data:
+                    received_bundle = item["bundle"]
+                    # Check if the bundle is already allocated
+                    if received_bundle.status=="allocated":
+                        raise serializers.ValidationError(f"Bundle {received_bundle.id} is already allocated")
+                    
                     FirstWashBundleSource.objects.create(
                         batch_for_first_wash=batch_for_first_wash,
-                        bundle=item["bundle"],
+                        bundle=received_bundle,
                         quantity=item["quantity"],
                     )
+                    
+                    # update the received bundle's status
+                    received_bundle.status = "allocated"
+                    received_bundle.save(update_fields=["status"])
 
             batch_for_first_wash.total_quantity = total_quantity
             batch_for_first_wash.save()
