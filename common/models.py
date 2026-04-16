1991fe5dee7dc9e9a764eefbe3fdd9c25f670043
Talha Taiyoub <talha.taiyoub@gmail.com>
2026-04-16 15:19:38 +0600

    create model MasterPlan and PlanningRouteSteps

--- common/models.py
+++ common/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+class MasterPlan(models.Model):
+    buyer = models.CharField(max_length=100)
+    style = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    updated_by = models.CharField(max_length=100)
+    last_update = models.DateTimeField(auto_now=True)
+    
+class PlanningRouteStep(models.Model):
+    master_plan = models.ForeignKey(
+        MasterPlan,
+        on_delete=models.CASCADE,
+        related_name='route_steps'
+    )
+    sequence = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    stage = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.planning_id} - {self.sequence} - {self.stage}"    
