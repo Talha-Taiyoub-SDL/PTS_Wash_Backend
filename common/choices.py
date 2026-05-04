35518f86f8d067fc6fcd0dbd05dc1d9970ea0376
Talha Taiyoub <talha.taiyoub@gmail.com>
2026-05-04 10:33:48 +0600

    Add GarmentUnitStatus model to facilitate multiple choices fields of GarmentUnit model

---
+++ common/choices.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class GarmentUnitStatus(models.TextChoices):
+    RECEIVED = "received", "Received"
+    REJECTED = "rejected", "Rejected"
+    WHISKER_IN = "whisker_in", "Whisker In"
+    WHISKER_OUT = "whisker_out", "Whisker Out"
+    BRUSH_IN = "brush_in", "Brush In"
+    BRUSH_OUT = "brush_out", "Brush Out"
+        
