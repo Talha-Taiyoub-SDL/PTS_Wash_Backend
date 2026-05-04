3b40a81c208eb6499aeb093996fd7df5b897437c
Talha Taiyoub <talha.taiyoub@gmail.com>
2026-05-04 15:48:00 +0600

    Create model TrackingHIstory for tracking individual garment

--- dry_process/models.py
+++ dry_process/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from common.models import GarmentUnit
+from .choices import Stage, Action
 
 # Create your models here.
+class TrackingHistory(models.Model):
+    garment_unit = models.ForeignKey(GarmentUnit, on_delete=models.CASCADE, related_name="histories")
+    stage = models.CharField(max_length=100, choices=Stage.choices)
+    action = models.CharField(max_length=100, choices=Action.choices)
+    scanned_at = models.DateTimeField(auto_now_add=True)
+    operator = models.CharField(max_length=100, null=True, blank=True)
