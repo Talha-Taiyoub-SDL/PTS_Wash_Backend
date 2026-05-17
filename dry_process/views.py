2049f5848418d7276553e14e11cfe2e2e1448901
Talha Taiyoub <talha.taiyoub@gmail.com>
2026-05-17 17:14:24 +0600

    feat: implement rejection handling in RejectionViewSet

--- dry_process/views.py
+++ dry_process/views.py
@@ -1,9 +1,27 @@
 from .models import TrackingHistory
-from .serializers import TrackingHistorySerializer
-from rest_framework.viewsets import ModelViewSet
+from .serializers import TrackingHistorySerializer, RejectionSerializer
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.response import Response
+from rest_framework import status, mixins
 
 # Create your views here.
 class TrackingHistoryViewSet(ModelViewSet):
     queryset = TrackingHistory.objects.all()
     serializer_class = TrackingHistorySerializer
+    
+class RejectionViewSet(ModelViewSet):
+    queryset = TrackingHistory.objects.all()
+    
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return RejectionSerializer
+        return TrackingHistorySerializer
+        
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        histories = serializer.save()
+        serializer = TrackingHistorySerializer(histories, many=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+            
     
