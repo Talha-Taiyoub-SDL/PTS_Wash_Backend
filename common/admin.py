97189401c2e085ba1f18103a4c9a593fcdec0618
Talha Taiyoub <talha.taiyoub@gmail.com>
2026-05-18 17:27:19 +0600

    feat: apply inline editing for the field status of Garment Unit in the admin panel

--- common/admin.py
+++ common/admin.py
@@ -1,19 +1,37 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
 @admin.register(models.MasterPlan)
 class MasterPlanAdmin(admin.ModelAdmin):
     list_display = ["id", "buyer", "style", "color", "last_update", "updated_by"]
 
+
 @admin.register(models.RouteStep)
 class RouteStepAdmin(admin.ModelAdmin):
-    list_display = ["id","master_plan", "sequence", "stage"]
-    
+    list_display = ["id", "master_plan", "sequence", "stage"]
+
+
 @admin.register(models.SubRouteStep)
 class SubRouteStepAdmin(admin.ModelAdmin):
-    list_display = ["id", "route_step", "sequence", "stage"]        
+    list_display = ["id", "route_step", "sequence", "stage"]
+
 
 @admin.register(models.GarmentUnit)
 class GarmentUnitAdmin(admin.ModelAdmin):
-    list_display = ["individual_barcode", "so", "mpo", "buyer", "style", "marker", "size", "shade", "color", "status", "received_at", "received_by"]
+    list_display = [
+        "individual_barcode",
+        "so",
+        "mpo",
+        "buyer",
+        "style",
+        "marker",
+        "size",
+        "shade",
+        "color",
+        "status",
+        "received_at",
+        "received_by",
+    ]
+    list_editable = ["status"]
