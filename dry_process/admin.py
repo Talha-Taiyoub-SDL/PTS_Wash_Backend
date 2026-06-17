325ef4eaa03edb503fd8a5fa09d72dd58fb3b495
Talha Taiyoub <talha.taiyoub@gmail.com>
2026-06-17 16:17:39 +0600

    feat: add Planning in the admin panel

--- dry_process/admin.py
+++ dry_process/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+
+@admin.register(models.Planning)
+class PlanningAdmin(admin.ModelAdmin):
+    list_display = ["id", "mpo", "color", "updated_by", "last_update"]
+    search_fields = ["mpo"]
+
+
 @admin.register(models.TrackingHistory)
 class TrackingHistoryAdmin(admin.ModelAdmin):
-    list_display = ["garment_unit", "stage", "action", "rejection_reason", "scanned_at", "operator"]
+    list_display = [
+        "garment_unit",
+        "stage",
+        "action",
+        "rejection_reason",
+        "scanned_at",
+        "operator",
+    ]
