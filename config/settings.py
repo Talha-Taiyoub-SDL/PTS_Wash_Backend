66f980ebd4a06c4571b79a5d9d5671b144ed0f13
Talha Taiyoub <talha.taiyoub@gmail.com>
2026-06-29 17:39:22 +0600

    chore: transfer database from sqlite to postgresql

--- config/settings.py
+++ config/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "pts_wash",
+        "USER": "pts_wash_user",
+        "PASSWORD": "square@pts_wash123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
